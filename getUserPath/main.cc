d0c4c6f8c91bd4ca4aa4706aa8a0ef1774ba7a26
xinxin-Esme <xinxin.wan@intel.com>
2022-07-12 13:49:04 +0800

    get user path in linux

--- getUserPath/main.cc
+++ getUserPath/main.cc
@@ -11,15 +11,18 @@
  * Build: g++ -o getUserPath
  * */
 
+
+// getuid(): to get the user id of current user
+// getpwuid to get the password entry(which includes the home directory) of the user
+// also, you could see pw->pw_name to get username, pw->pw_passwd get password and etc.
 struct passwd *pw = getpwuid(getuid());
 const char *homedir = pw->pw_dir;
 
 int main() {
 
 	using namespace std;
-    cout << "HOME : " << homedir << endl;
-
-    return 0;
+	cout << "HOME : " << homedir << endl;
+       	return 0;
 }
 
 
